0d931d6d714c5b62c4dca6c4b74777c99f952fc1
Himanshu <himanshukar1810@gmail.com>
2023-08-20 14:37:12 +0530

    Learned multiple types of Selectors in jQuery and jquery methods like hide, css

---
+++ selectors/script.js
@@ -0,0 +1,14 @@
+// $('.heading').hide();
+
+// $('#heading1').hide();
+
+// $('#para1 span').css('color', 'red');
+
+// $('ul#list li:first').css('color', 'red');
+// $('ul#list li:last').css('color', 'green');
+// $('ul#list li:even').css('background-color', 'yellow');
+
+// $("input[type='text']").hide();
+// $(":submit").hide();
+// $(":button").hide();
+
